870e3f9ac0e0ce6a8ae93e1f5c7d7367b7d4addb
Miguel Sanchez <ing.miguels@gmail.com>
2023-03-10 18:05:42 -0600

    Changed message and fixed error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
@@ -14,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React is cool!
         </a>
       </header>
     </div>
