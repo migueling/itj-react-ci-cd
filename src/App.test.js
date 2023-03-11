000b8461d24c46504f57b2f75491a527a6df6301
Miguel Sanchez <ing.miguels@gmail.com>
2023-03-10 18:24:06 -0600

    fixed

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders React is cool! link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/React is cool!/i);
+  const linkElement = screen.getByText(/Thanks everyone!/i);
   expect(linkElement).toBeInTheDocument();
 });
